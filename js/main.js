5bdaedbc2cfe23b5cc8bf0f5b7868e51003b4f47
Ilya <wickedbun@yandex.ru>
2022-01-30 14:23:09 +0500

    Fixed setInterval

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 import {intervalToDuration, formatDuration} from 'date-fns';
 import Cookies from 'js-cookie'
 
+let index = 0;
+
 if (Cookies.get('endTime')) {
   document.querySelector('.input').value = Cookies.get('endTime')
-  updateTime(Cookies.get('endTime'))
+  updateTime(Cookies.get('endTime'), index++)
 };
 
 document.querySelector('.form').addEventListener('submit', function() {
   endTime = this.firstElementChild.value;
   Cookies.set('endTime', endTime, {expires: 1/24})
-  updateTime(endTime)
+  updateTime(endTime, index++)
 });
 
 function getInterval(end) {
@@ -19,8 +21,10 @@ function getInterval(end) {
   });
 }
 
-function updateTime(endTime) {
-  setInterval(() => {
+function updateTime(endTime, ind) {
+  const timer = setInterval(() => {
+    if (ind + 1 != index) clearTimeout(timer);
+
     document.querySelector('.result').textContent = formatDuration(getInterval(endTime));
   }, 1000);
 }
